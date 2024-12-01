bcdcb8c7f39c54b7851e8f414d2e2a8d6d6c32dc
introsent <ivans.minajevs@gmail.com>
2024-12-01 16:06:43 +0100

    feat: add Day 1 Part 2 solution

---
+++ day_1/day_1_2.py
@@ -0,0 +1,14 @@
+lrow_list = []
+rrow_list = []
+sum = 0
+with open('data.txt', 'r') as file:
+    for line in file:
+        lrow_value, rrow_value = map(int, line.split())
+        lrow_list.append(lrow_value)
+        rrow_list.append(rrow_value)
+
+sum = 0
+for l in lrow_list:
+    sum += l * rrow_list.count(l)
+
+print (sum)
