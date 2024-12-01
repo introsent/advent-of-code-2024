c76ae916ccb3d5d9c4b0634b4187e4d5edd8a221
introsent <ivans.minajevs@gmail.com>
2024-12-01 16:01:23 +0100

    update: switch l and r raw values

--- day_1/day_1_1.py
+++ day_1/day_1_1.py
@@ -5,9 +5,9 @@ lrow_list = []
 sum = 0
 with open('data.txt', 'r') as file:
     for line in file:
-        rrow_value, lrow_value = map(int, line.split())
-        bisect.insort(rrow_list, rrow_value)
+        lrow_value, rrow_value = map(int, line.split())
         bisect.insort(lrow_list, lrow_value)
+        bisect.insort(rrow_list, rrow_value)
 
 for r, l in zip(rrow_list , lrow_list):
     sum += abs(r - l)
