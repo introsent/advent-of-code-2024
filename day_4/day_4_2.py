2eddfce8d9b18dac369dc912ffbebcbbc60f3e72
introsent <ivans.minajevs@gmail.com>
2024-12-04 08:49:03 +0100

    feat: add Day 4 Part 2 solution

---
+++ day_4/day_4_2.py
@@ -0,0 +1,17 @@
+chars_matrix = []
+sum = 0
+with open('day_4/data.txt', 'r') as file:
+    for line in file:
+        line_to_char_arr = [char for char in line.replace("\n", "")]
+        chars_matrix.append(line_to_char_arr)
+            
+for x, xv in enumerate(chars_matrix):
+    for y, yv in enumerate(xv):
+        if (y < len(xv) - 2 and x < len(chars_matrix) - 2):
+            l1 = chars_matrix[x][y] + chars_matrix[x+1][y+1] + chars_matrix[x+2][y+2]
+            l2 = chars_matrix[x][y+2] + chars_matrix[x+1][y+1] + chars_matrix[x+2][y]
+            if ((str(l1) == "MAS" or str(l1) == "SAM") and (str(l2) == "MAS" or str(l2) == "SAM")):
+                sum+=1
+
+
+print(sum)
