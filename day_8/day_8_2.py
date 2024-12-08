d36f4d8b30c1a035a92c8bb898cff594bbad19cc
introsent <ivans.minajevs@gmail.com>
2024-12-08 16:22:48 +0100

    feat: add Day 8 Part 2 solution

--- day_8/day_8_2.py
+++ day_8/day_8_2.py
@@ -1,7 +1,7 @@
 chars_matrix = []
 sum = 0
 letter_positions = {}
-with open('day_8/data.txt', 'r') as file:
+with open('day_8/test_data.txt', 'r') as file:
     for row, line in enumerate(file):
         line_char = []
         for col, char in enumerate(line.replace("\n", "")):
@@ -25,19 +25,25 @@ for key in letter_positions:
         row_offset = first[0] - second[0]
         col_offset = first[1] - second[1]
 
-        row_first = first[0] + row_offset 
-        col_first = first[1] + col_offset
+        row_first = first[0]
+        col_first = first[1] 
 
-        if (row_first in range(0, len(chars_matrix)) and col_first in range(0, len(chars_matrix[0]))):
+        while (row_first in range(0, len(chars_matrix)) and col_first in range(0, len(chars_matrix[0]))):
             if (grid[row_first][col_first] == '.'):
                 grid[row_first][col_first] = '#'
                 sum += 1
+            row_first += row_offset  
+            col_first += col_offset
 
-        row_second = second[0] - row_offset 
-        col_second = second[1] - col_offset
-        if (row_second in range(0, len(chars_matrix)) and col_second in range(0, len(chars_matrix[0]))):
+        row_second = second[0] 
+        col_second = second[1] 
+        while (row_second in range(0, len(chars_matrix)) and col_second in range(0, len(chars_matrix[0]))):
             if (grid[row_second][col_second] == '.'):
                 grid[row_second][col_second] = '#'
                 sum += 1
-           
+            row_second -= row_offset  
+            col_second -= col_offset
+
+for line in grid:
+    print(line)           
 print(sum)
